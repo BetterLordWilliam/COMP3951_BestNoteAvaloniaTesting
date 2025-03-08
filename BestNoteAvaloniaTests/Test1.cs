09907ebf8e60db1ac8ba63ec77b0ecdf87e0946e
BetterLordWilliam <willjordan05@icloud.com>
2025-03-07 18:06:34 -0800

    WO: Fixed the project setup to include the unit tests I started writing for the BestNoteFIleSYstem servcie,

---
+++ BestNoteAvaloniaTests/Test1.cs
@@ -0,0 +1,48 @@
+﻿using BestNoteAvalonia.Services;
+using System.Diagnostics;
+
+namespace BestNoteAvaloniaTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        public BestNoteFileManagerService bs = new BestNoteFileManagerService();
+
+        /// <summary>
+        /// Test for adding a folder to the file system.
+        /// </summary>
+        [TestMethod]
+        public void TestRootDirectory()
+        {
+            // Get the root contents of the parent file
+            DirectoryInfo output = bs.GetRootContents();
+            Assert.IsNotNull(output);
+            Debug.WriteLine($"Best note app data directory {output.FullName}");
+        }
+
+        /// <summary>
+        /// Test for validatitng the root directory was created.
+        /// </summary>
+        [TestMethod]
+        public void TestNoteDirectory()
+        {
+            // Get the root contents of the parent file
+            TestRootDirectory();
+            DirectoryInfo output = bs.GetNotesContents();
+            Assert.IsNotNull(output);
+            Debug.WriteLine($"Best note notes directory {output.FullName}");
+        }
+
+        /// <summary>
+        /// Test for adding folders to the file system.
+        /// </summary>
+        [TestMethod]
+        public void TestAddFolder()
+        {
+            TestNoteDirectory();
+            DirectoryInfo output = bs.AddFolder("Will Testing Notes");
+            Assert.IsNotNull(output);
+            Debug.WriteLine($"Best note new folder directory {output.FullName}\nBest note new folder name {output.Name}");
+        }
+    }
+}
